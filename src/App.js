d7387e53f2657a1748af1c2406b03901cb7af000
gptinlin <garrett.tinlin@gmail.com>
2021-01-25 15:25:59 -0800

    9th commit added js to app file

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Tasklist from './Tasklist';
+import Tasks from './Tasks';
+import Nav from './Nav';
+import Contact from './Contact';
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
+import { queryHelpers } from '@testing-library/react';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Nav2 />
+        <Switch>
+        <Route path="/tasklist" exact component={Tasklist} />
+        <Route path="/tasks" exact component={Tasks} />
+        <Route path="/contact" exact component={Contact} />
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
 
