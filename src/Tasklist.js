efbd2a47be508078ed495c52d702e243277f9805
gptinlin <garrett.tinlin@gmail.com>
2021-01-25 15:29:37 -0800

    10th commit- added tasklist

---
+++ src/Tasklist.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './App.css';
+import Item from './Item';
+
+
+export default function Tasklist() {
+    return (
+        <div>
+            <Item />
+        </div>
+    )
+}
