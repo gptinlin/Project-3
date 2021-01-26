75d15c1a6d904d0941da7bbfb288312a3ef9615e
gptinlin <garrett.tinlin@gmail.com>
2021-01-25 16:27:13 -0800

    13th commit added script to contact file

--- src/Contact.js
+++ src/Contact.js
@@ -3,3 +3,22 @@ import './App.css';
 import ContactTest from './ContactTest';
 
 class Contact extends React.Component {
+    render(){
+
+        return (
+            <div className="App">
+                {/* <form>
+                    <div>First Name:<input type = 'text' name = 'firstName' value = {this.state.firstName} onChange = {this.handleOnChange}/></div><br></br>
+                    <div>Last Name: <input type = 'text' name = 'lastName' value = {this.state.lastName} onChange = {this.handleOnChange}/></div> <br></br>
+                    <div>Email: <input type = 'text' name = 'eMail' value = {this.state.eMail} onChange = {this.handleOnChange}/> </div><br></br>
+                    <div>Comment: <input type = 'text' name = 'comment' value = {this.state.comment} onChange = {this.handleOnChange}/></div>
+                    </form>
+                <button>Submit</button> */}
+                <ContactTest/>
+            </div>
+          
+        );
+    }
+  }
+  
+  export default Contact;
